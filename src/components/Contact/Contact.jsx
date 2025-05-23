7b389f0f8092802501ef6e87dd27fd59b892b785
Rebecca-254 <gitaurebecca407@gmail.com>
2025-05-23 13:20:36 +0300

    changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => (
       <p>📞 <a href="tel:+254746230020">0746 230 020</a></p>
       <p>📧 <a href="mailto:gitaurebecca407@gmail.com">gitaurebecca407@gmail.com</a></p>
       <p>🐱 <a href="https://github.com/Rebecca-254" target="_blank" rel="noopener noreferrer">GitHub</a></p>
-      <p>🔗 <a href="https://linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+      <p>🔗 <a href="https://www.linkedin.com/in/rebecca-gitau-093689366?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">LinkedIn</a></p>
     </div>
   </section>
 );
